6af967c6c7a2554384576465651b618c87803746
“youichifukumoto <fukumoto1212@gmail.com>
2022-11-12 09:16:41 +0900

    softDelete

--- resources/views/layouts/admin-navigation.blade.php
+++ resources/views/layouts/admin-navigation.blade.php
@@ -18,8 +18,12 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                     <x-nav-link :href="route('admin.owners.index')" :active="request()->routeIs('admin.owners.index')">
-                        オーナー管理
+                        メーカー管理
                     </x-nav-link>
+                    <x-nav-link :href="route('admin.expired-owners.index')" :active="request()->routeIs('adminexpired-owners.index')">
+                        期限切れメーカー一覧
+                    </x-nav-link>
+
                 </div>
             </div>
 
