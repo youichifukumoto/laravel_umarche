44338582b2ea8d852431f90104f01120ad5e175a
“youichifukumoto <fukumoto1212@gmail.com>
2022-11-12 11:58:30 +0900

    ルート、ビュー編集

--- routes/admin.php
+++ routes/admin.php
@@ -21,11 +21,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('admin.welcome');
-});
+// Route::get('/', function () {
+//     return view('admin.welcome');
+// });
 
-Route::resource('owners', OwnersController::class) ->middleware('auth:admin');
+Route::resource('owners', OwnersController::class) ->middleware('auth:admin')->except(['show']);
 
 Route::prefix('expired-owners')-> middleware('auth:admin')->group(function(){
 Route::get('index', [OwnersController::class, 'expiredOwnerIndex'])->name('expired-owners.index');
