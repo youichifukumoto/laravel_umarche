fd4491ec859863805f71ccb5e4199965e5cf9592
“youichifukumoto <fukumoto1212@gmail.com>
2022-11-20 21:17:01 +0900

    ログアウト時のリダイレクト修正

--- app/Http/Controllers/Owner/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Owner/Auth/AuthenticatedSessionController.php
@@ -51,6 +51,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/owner');
+        return redirect('/owner/login');
     }
 }
